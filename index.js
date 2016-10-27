5a8e6bd6ca2142ac17f0c7a18df2149ee1da1d22
orthagonal <orthagonal1@gmail.com>
2016-10-26 23:53:41 -0500

    first commit (#1)
    basic version, set up so it will be easy to add the future features.

---
+++ index.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+'use strict';
+const Watcher = require('./lib/DockerWatcher.js');
+const yargs = require('yargs');
+const os = require('os');
+const Logr = require('logr');
+const argv = yargs
+.option('cpu_threshold', {
+  describe: 'threshold above which CPU is considered in elevated state',
+  default: 50,
+  type: 'number'
+})
+.option('cpu_duration', {
+  describe: 'duration in milliseconds that CPU can be in elevated state before triggering a warning',
+  default: 50,
+  type: 'number'
+})
+.option('machine', {
+  describe: 'an identifier this machine can use when warning of elevated state',
+  default: os.hostname,
+  type: 'string'
+})
+.help('h')
+.alias('h', 'help')
+.env(true)
+.argv;
+
+const log = new Logr({
+  defaultTags: [argv.machine]
+});
+const options = {
+  cpu: {
+    threshold: argv.cpu_threshold,
+    duration: argv.cpu_duration
+  }
+};
+if (!argv.h && !argv.help) {
+  const watcher = new Watcher(argv.machine, options, log);
+  watcher.startAll();
+  const runForever = () => {
+    setTimeout(runForever, 120000);
+  };
+  runForever();
+}
