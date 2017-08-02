805d455408ddf6a0dc4382f23712092da5af39ab
orthagonal <orthagonal1@gmail.com>
2017-08-02 17:26:48 -0500

    Add name to tags (#8)
    * add name to tags
    
    * add name to tags

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const get = require('lodash.get');
 
 const verboseMode = process.env.VERBOSE === '1';
 
-const colors = {
+const tagColors = {
   start: 'bgGreen',
   stop: 'bgRed'
 };
@@ -20,7 +20,7 @@ const logOptions = {
       options: {
         timestamp: false,
         appColor: true,
-        colors
+        tagColors
       }
     }
   }
@@ -63,6 +63,7 @@ const handleMessage = (message, tags) => {
   // non-verbose mode logs matching tags for 'start' and 'stop' events:
   const name = get(message, 'Actor.Attributes.name', '');
   if (name) {
+    tags.unshift(name);
     for (let i = 0; i < slackNotify.length; i++) {
       const match = name.match(slackNotify[i]);
       if (match && match.length > 0) {
